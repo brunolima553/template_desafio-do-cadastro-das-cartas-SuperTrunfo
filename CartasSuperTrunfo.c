371691f8fcc215f07eb9a4a10a7ce7a7f45d84e0
brunolima553 <brunolimasd553@gmail.com>
2026-04-16 14:52:45 +0000

    Inicio no deafio supertrunfo Declarei todoas as variaveis,com todo o fluxograma de entrada e saida de dados. No entanto ao compilar apresenta falha (nome da cidade1 não aparece e o codigo1 aparece codigo1+codigo2.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,100 +1,87 @@
-#include <stdio.h> //incluir biblioteca
+#include<stdio.h>//inccluir biblioteca
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das cartas
+//Desafio Super Trunfo 
+//Tema 1 Cadastro de Cartas
 
+ int main(){
 
-int main() {
-  //Declarando todas as variaveis.
-   char Estado1 ,Estado2;
-   char Codigo1[3] ,Cidade1[50] ,Codigo2[3] ,Cidade2[60];
-   int  Populacao1,Turistico1,Populacao2,Turistico2;
-   float Area1, PIB1, Area2, PIB2;
+    //Declarando as variaveis
+    char estado1, estado2;
+    char codigo1[03],codigo2[03],cidade1[30],cidade2[40];
+    int populacao1,populacao2,turistico1,turistico2;
+    float area1, area2, pib1, pib2;
 
-   printf("Vamos começar o cadastramento das cartas!  \n");//interação
-   printf("Carta 1   \n"); // interação
-   printf("Digite o estado com um caractere de 'A' á 'H'    \n");// saída de dados
-   scanf(" %c",&Estado1); //entrada de dados 
+    printf("Vamos começar o cadastramento de cartas!  \n");//interação
+    printf("Carta 1  \n");//interação
+    printf("Digite o Estado com um caracter de 'A' á 'H'  \n");//Saida de dados 
+    scanf("%c",&estado1);//Entrada de dados
 
-   printf("Digite o código da carta com três caracteres!   \n");
-   scanf("%s",Codigo1);
+    printf("Digite o codigo da carta com três caracteres!  \n");
+    scanf("%s",codigo1);
 
-   printf("Digite o nome da cidade!   \n");
-   scanf("%s",Cidade1);//string não precisa do &
+    printf(" Digie o nome da cidade!  \n");
+    scanf("%s",cidade1);
 
-   printf("Digite a população!   \n");
-   scanf("%d",&Populacao1);
+    printf("Digite á populacao!  \n");
+    scanf("%d",&populacao1);
 
-   printf("Digite a Área km2!  \n");
-   scanf("%f",&Area1);
+    printf("Digite a Área em km2! \n");
+    scanf("%f",&area1);
 
-   printf("Digite o PIB!  \n");
-   scanf("%f",&PIB1);
+    printf("Digite o PIB!  \n");
+    scanf("%f",&pib1);
 
-   printf("Digite o n° de pontos turisticos!  \n");
-   scanf("%d",&Turistico1);
+    printf("Digite o n° de pontos turisticos!  \n");
+    scanf ("%d",&turistico1);
 
-   printf("Primeira carta concluida!\n""Vamos continuar!    \n");
+    printf("Primeira carta concluída! \n ");
+    printf("Vamos continuar!  \n");
 
-   printf("Carta 2  \n"); // interação
-   printf("Digite o estado com um caractere de 'A' á 'H'   \n");// saída de dados
-   scanf(" %c",&Estado2); //entrada de dados 
+    printf("Carta 2  \n");
+    printf("Digite o  estado com um caracter de 'A' á 'H'  \n");//saísa de dados
+    scanf(" %c",&estado2);//entrada de dados
 
-   printf("Digite o código da carta com três caracteres!\n");
-   scanf("%s",Codigo2);
+    printf("Digite o codigo da carta com três caracteres!  \n");
+    scanf("%s",codigo2);
 
-   printf("Digite o nome da cidade!\n");
-   scanf("%s",Cidade2);
+    printf(" Digite o nome da cidade!  \n");
+    scanf("%s",cidade2);
 
-   printf("Digite a população!\n");
-   scanf("%d",&Populacao2);
+    printf("Digite a populacao! \n");
+    scanf("%d",&populacao2);
 
-   printf("Digite a Área km2!\n");
-   scanf("%f",&Area2);
+    printf("Digite a Área em km2!  \n");
+    scanf("%f",&area2);
 
-   printf("Digite o PIB!\n");
-   scanf("%f",&PIB2);
+    printf("Digite o PIB!  \n");
+    scanf("%f",&pib2);
 
-   printf("Digite o n° de pontos turisticos!\n");
-   scanf("%d",&Turistico2);
+    printf("Digite o n° de pontos turisticos!  \n");
+    scanf("%d",&turistico2);
 
-   printf("Concluido,acompanhe o resultado!\n");
-   printf("\n");
-   printf("\n");
+    printf("Concluído,acompanhe o resultado!  \n");
+    printf("\n");
+    printf("\n");
 
+    printf("Carta 1  \n");
+    printf("Estado:%c \n",estado1);
+    printf("Código:%s \n",codigo1);
+    printf("Cidade:%s \n",cidade1);
+    printf("População:%d \n",populacao1);
+    printf("Área:%f \n",area1);
+    printf("PIB:%f \n",pib1);
+    printf("Pontos Turisticos:%d \n",turistico1);
+    printf("\n");
+    printf("\n");
 
-   printf("Carta 1\n");
-   printf("Estado:%c  \n",Estado1);
-   printf("Código:%s  \n",Codigo1);//não entendo pq não aparece 
-   printf("Cidade:%s  \n",Cidade1);//não entendo pq não aprece
-   printf("População:%d  \n",Populacao1);
-   printf("Área:%f  \n",Area1);
-   printf("PIB:%f  \n",PIB1);
-   printf("Pontos Turisticos:%d  \n",Turistico1);
+    printf("Carta 2 \n");
+    printf("Estado:%c \n",estado2);
+    printf("Código:%s \n",codigo2);
+    printf("Cidade:%s \n",cidade2);
+    printf("População:%d \n",populacao2);
+    printf("Área:%f \n",area2);
+    printf("PIB:%f \n",pib2);
+    printf("Pontos Turisticos:%d  \n",turistico2); //finalizado,alguns erros quando aparece os dados das cartas 
 
-   printf("\n");
-   printf("\n");
-
-   printf("Carta 2\n");
-   printf("Estado:%c  \n",Estado2);
-   printf("Código:%s  \n",Codigo2);
-   printf("Cidade:%s  \n",Cidade2);
-   printf("População:%d  \n",Populacao2);
-   printf("Área:%f  \n",Area2);
-   printf("PIB:%f  \n",PIB2);
-   printf("Pontos Turisticos:%d  \n",Turistico2);
-
-
-  
-
-
-
-
-
-
-
-
-
-
-return 0;
-} 
+  return 0;
+ }
